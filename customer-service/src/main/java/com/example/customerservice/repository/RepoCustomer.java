d48d2b4ee70628b0cef328b97446f9aaa3dfa013
Github-moncef <bahjamoncef66@gmail.com>
2023-12-20 15:22:50 +0100

    assosiation healty

--- customer-service/src/main/java/com/example/customerservice/repository/RepoCustomer.java
+++ customer-service/src/main/java/com/example/customerservice/repository/RepoCustomer.java
@@ -2,6 +2,8 @@ package com.example.customerservice.repository;
 
 import com.example.customerservice.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 
+@RepositoryRestController
 public interface RepoCustomer extends JpaRepository<Customer,Long> {
 }
